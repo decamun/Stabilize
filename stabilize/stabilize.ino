a079dd7566fb604f3f67981bc85a4c7b5f069321
Mitch Fogelson <mfog@seas.upenn.edu>
2017-03-28 22:19:38 -0400

    fuck

--- stabilize/stabilize.ino
+++ stabilize/stabilize.ino
@@ -4,7 +4,7 @@
 
 #include <MemoryFree.h>
 
-#define LOGGING 0
+#define LOGGING 1
 //0: no logging
 //1: serial logging,
 //2: SD card logging
@@ -62,11 +62,11 @@ void syncMotor(int motNum);
 void setup() {
   pinMode(kLedPin, OUTPUT);
   dataLoggingSetup();
-  imuSetup();
+//  imuSetup();
   motControlSetup();
-  motSetup(1, 10,0,0);
-  motSetup(2, 10,0,0);
-  motSetup(3, 10,0,0);
+  motSetup(1, 100,0,0);
+  motSetup(2, 100,0,0);
+  motSetup(3, 100,0,0);
   
   if(!LOOP_LOGGING) {
     setLogPermission(false);
@@ -85,37 +85,38 @@ void loop() {
   syncMotor(2);
   syncMotor(3);
 
-  setVal(1, ANGLE, 0);
-  setVal(2, ANGLE, 0);
+  setVal(1, ANGLE, 1.5);
+  setVal(2, ANGLE, 1.5);
   setVal(3, ANGLE, 0);
   
   
 
-  //imu
-  if(!updateImu()) {
-    stronglogln("Something went wrong with the IMU.");
-    return; //skip the cycle if the imu doesn't update correctly
-  }
+//  //imu
+//  if(!updateImu()) {
+//    stronglogln("Something went wrong with the IMU.");
+//    return; //skip the cycle if the imu doesn't update correctly
+//  }
 
   logln("Compiling output...");
 
   static long ct = 0;
-  String string = String(ct) + String(",") + String(loop_time);
+  String string = String(ct) + String(",") + String(getVal(2, ANGLE)) + String(getVal(3, ANGLE)) + String(getVal(1, ANGLE));;
+//  String string = String(ct) + String(",") + String(loop_time) + String(",") + String(imu.roll) + String(",") + String(imu.pitch) + String(",") + String(imu.yaw) + String(",") + String(imu.ax) + String(",") + String(imu.ay) + String(",") + String(imu.az) + String(",") + String(getVal(1, ANGLE)) + String(",") + String(getVal(2, ANGLE)) + String(",") + String(getVal(3, ANGLE));
   ct++;
-  string += String(",") + String(imu.roll);
-  string += String(",") + String(imu.pitch);
-  string += String(",") + String(imu.yaw);
-  string += String(",") + String(imu.ax);
-  string += String(",") + String(imu.ay);
-  string += String(",") + String(imu.az);
-  string += String(",") + String(getVal(1, ANGLE));
-  string += String(",") + String(getVal(2, ANGLE));
-  string += String(",") + String(getVal(3, ANGLE));
-
-  string += String(",") + String(freeMemory());
-  string += String(",") + String(freeMemory());
-
-  //output
+//  string += String(",") + String(imu.roll);
+//  string += String(",") + String(imu.pitch);
+//  string += String(",") + String(imu.yaw);
+//  string += String(",") + String(imu.ax);
+//  string += String(",") + String(imu.ay);
+//  string += String(",") + String(imu.az);
+//  string += String(",") + String(getVal(1, ANGLE));
+//  string += String(",") + String(getVal(2, ANGLE));
+//  string += String(",") + String(getVal(3, ANGLE));
+//
+//  string += String(",") + String(freeMemory());
+//  string += String(",") + String(freeMemory());
+//
+//  //output
   dataln(string);
   logln(String("Data Output: ")+string);
 }
